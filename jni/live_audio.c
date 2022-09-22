5f1e05d1b10245a4e645d85204b72d42d3f86024
markusstadtmann.ext@levatis.com <Tornado1986!>
2022-09-22 15:17:55 +0200

    release version 1.2.3

--- jni/live_audio.c
+++ jni/live_audio.c
@@ -10,6 +10,22 @@ void initLibs() {
   if (djiGUILib == 0) {
     djiGUILib = dlopen("/system/lib/libtp1801_gui.so", 1);
   }
+
+  if (ui_config == 0) {
+    ui_config = (uint32_t *)*(uint32_t *)((int)getSettings() + 0xe4);
+  }
+
+  if (gs_get_uav_hardware_version) {
+    gs_get_uav_hardware_version = (void *)*(uint32_t *)((int) ui_config + 0x3a8);
+  }
+
+  if (hardware_info) {
+    hardware_info = (uint32_t *)*(uint32_t *)((int) ui_config + 0x4c);
+  }
+
+  if (gs_enable_audio_liveview) {
+    gs_enable_audio_liveview = (void *)*(uint32_t *)((int) ui_config + 0x3e0);
+  }
 }
 
 uint32_t getTimeout(void* this) {
@@ -38,15 +54,14 @@ uint32_t getSettings() {
    }
 }
 
-void setLiveAudio(bool enable, void *hardware_info, void *ui_config){
+void setLiveAudio(bool enable){
     restart = enable;
-	gs_enable_audio_liveview = (void *)*(uint32_t *)((int) ui_config + 0x3e0);
 	printf("update live audio: %d\n", enable);
 	gs_enable_audio_liveview(hardware_info, enable);
 	printf("update live audio\n");
 }
 
-bool isAirUnitLite(void *hardware_info){
+bool isAirUnitLite(){
 	char hw_ver[16];
 	memset(hw_ver, 0, 0x10);
 	gs_get_uav_hardware_version(hardware_info, hw_ver);
@@ -56,17 +71,18 @@ bool isAirUnitLite(void *hardware_info){
 	return false;
 }
 
+void updateConnection() {
+    gs_modem_get_link_state_wrap = (void *)*(uint32_t *)((int) ui_config + 0x228);
+    connection = GS_LINK_STAT_UKNOWN;
+    gs_link_stat_t *connection_status = &connection;
+    gs_modem_get_link_state_wrap(hardware_info, connection_status);
+}
+
 int32_t _ZN19GlassRacingChnlMenu7timeOutEv(void* this){
     initLibs();
-	uint32_t *ui_config = (uint32_t *)*(uint32_t *)((int)getSettings() + 0xe4);
-	gs_get_uav_hardware_version = (void *)*(uint32_t *)((int) ui_config + 0x3a8);
-	uint32_t *hardware_info = (uint32_t *)*(uint32_t *)((int) ui_config + 0x4c);
 
-	if(!isAirUnitLite(hardware_info)){
-		gs_modem_get_link_state_wrap = (void *)*(uint32_t *)((int) ui_config + 0x228);
-		gs_link_stat_t connection = GS_LINK_STAT_UKNOWN;
-		gs_link_stat_t *connection_status = &connection;
-		gs_modem_get_link_state_wrap(hardware_info, connection_status);
+	if(!isAirUnitLite()){
+		updateConnection();
 
 		if(connection != GS_LINK_STAT_NORMAL){
 		   clock_gettime(CLOCK_MONOTONIC, &prev);
@@ -76,12 +92,12 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv(void* this){
 
 		if ((now.tv_sec - prev.tv_sec) > 9) {
 		   if(!restart  && connection == GS_LINK_STAT_NORMAL){
-		     setLiveAudio(true, hardware_info, ui_config);
+		     setLiveAudio(true);
 		   }
 		}
 
 		if(restart && connection == GS_LINK_STAT_LOST){
-		  setLiveAudio(false, hardware_info, ui_config);
+		  setLiveAudio(false);
 		}
 	}
 	return getTimeout(this);
--- jni/live_audio.h
+++ jni/live_audio.h
@@ -11,6 +11,9 @@ static int32_t (* gs_modem_get_link_state_wrap)(void *, enum gs_link_stat_t *) =
 static uint32_t (*gs_enable_audio_liveview)(void *_gs_info, bool b_enable) = 0;
 static uint32_t (*gs_get_uav_hardware_version)(void *_gs_info, char *_hw_ver) = 0;
 static void *djiGUILib = 0;
+static uint32_t *hardware_info = 0;
+static uint32_t *ui_config = 0;
+static gs_link_stat_t connection;
 
 struct timespec prev;
 struct timespec now;
@@ -19,6 +22,6 @@ static uint32_t (* timeout)(void *this) = 0;
 static uint32_t (* settings)() = 0;
 uint32_t getTimeout(void *this);
 uint32_t getSettings();
-void setLiveAudio(bool enable, void *hardware_info, void *ui_config);
-bool isAirUnitLite(void *hardware_info);
+void setLiveAudio(bool enable);
+bool isAirUnitLite();
 void initLibs();
