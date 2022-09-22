b2c7732910ddf75f3fab84ede247ebde2afebaa8
markusstadtmann.ext@levatis.com <Tornado1986!>
2022-09-22 14:16:35 +0200

    rollback v1

--- jni/live_audio.h
+++ jni/live_audio.h
@@ -10,19 +10,13 @@ typedef enum gs_link_stat_t {
 static int32_t (* gs_modem_get_link_state_wrap)(void *, enum gs_link_stat_t *) = 0;
 static uint32_t (*gs_enable_audio_liveview)(void *_gs_info, bool b_enable) = 0;
 static uint32_t (*gs_get_uav_hardware_version)(void *_gs_info, char *_hw_ver) = 0;
-static uint32_t *hardware_info = 0;
-static gs_link_stat_t connection;
-
 static void *djiGUILib = 0;
 
-struct timespec last;
-struct timespec now;
+struct timespec start, now;
 static bool restart = false;
 static uint32_t (* timeout)(void *this) = 0;
 static uint32_t (* settings)() = 0;
 uint32_t getTimeout(void *this);
 uint32_t getSettings();
-void initLibs();
-void updateConnection();
-void setLiveAudio(bool enable);
-bool isAirUnitLite();
+void setLiveAudio(bool enable, void *hardware_info, void *ui_config);
+bool isAirUnitLite(void *hardware_info);
